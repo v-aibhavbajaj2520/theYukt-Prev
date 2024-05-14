5dc5840ec853e7d40000f14c462d9ba0f6606f00
vaibhavraj072 <146511501+vaibhavraj072@users.noreply.github.com>
2024-05-15 04:43:43 +0530

    conflicts

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vaibh\OneDrive\Desktop\theYukt\myenv
+set VIRTUAL_ENV=C:\Users\vaibh\OneDrive\Documents\GitHub\theYukt\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
